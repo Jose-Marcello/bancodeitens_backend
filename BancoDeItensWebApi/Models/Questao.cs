aa2568a89429fa602711aedab10bf9642e77ea3d
José Marcello <josemarcellogardeldealemar@gmail.com>
2025-11-02 13:00:53 -0300

    Projeto V2 limpo: Configuração final de Azure SQL e ACA

---
+++ BancoDeItensWebApi/Models/Questao.cs
@@ -0,0 +1,13 @@
+﻿// Localização: Models/Questao.cs
+// Modelo Mínimo Viável (MMV) para o teste de conectividade
+namespace BancoDeItensWebApi.Models
+{
+    public class Questao
+    {
+        public int Id { get; set; }
+
+        // A Pergunta / Enunciado
+        public required string Descricao { get; set; }
+    }
+
+}
