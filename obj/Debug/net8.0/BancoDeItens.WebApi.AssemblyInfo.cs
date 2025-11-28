dde273f3f180971864f372614e4cb01bd0b22ec6
Jose-Marcello <josemarcellogardeldealemar@gmail.com>
2025-11-28 14:28:09 -0300

    Troca do Dockerfile para a raiz da Soution

--- obj/Debug/net8.0/BancoDeItens.WebApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/BancoDeItens.WebApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BancoDeItens.WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+241e948376d99dbb25970e71b329cd5d6f4673ad")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+07eaad86607e225fd4afa324f036bd6b48c9ff0b")]
 [assembly: System.Reflection.AssemblyProductAttribute("BancoDeItens.WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BancoDeItens.WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
